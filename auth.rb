942bb332796c06bc7963761f0a555047f7f703f4
Ardian Bahtiarsyah <ardian@phybbit.com>
2019-10-14 09:24:34 +0700

    remove `present?`

--- auth.rb
+++ auth.rb
@@ -1,7 +1,7 @@
 class Authentication
   def auth
     user = User.find_by(username: params[:username])
-    if user.present? && user.check_password(params[:password])
+    if user && user.check_password(params[:password])
       render json: user
     else
       render json: { errors: ["wrong username or password"] }, status: :unauthorized
